baccb78825bc75bf94ffb94c1debd47299296be2
anyfactor <abdullah.abd.398@gmail.com>
2019-07-30 01:29:45 +0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todos</h1>
+    <input type="text" v-model="todoName" @keyup.enter="addTodo">
+    <ul>
+      <li v-for="todo of todos" :key="todo.id">
+        {{todo.title}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+const baseURL = "https://jsonplaceholder.typicode.com/todos?_limit=5"
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      todoName: ''
+    }
+  },
+  async created() {
+    try {
+      const res = await axios.get(baseURL)
+
+      this.todos = res.data;
+    } catch(e) {
+      console.error(e)
+    }
+  },
+  methods: {
+    addTodo(newTodo){
+      const {title, completed} = newTodo;
+      axios.post(baseURL, {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, newTodo])
+        .catch(err => console.log(err))
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
